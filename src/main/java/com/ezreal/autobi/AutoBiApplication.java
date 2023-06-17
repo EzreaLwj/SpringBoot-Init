cddabbc06020471b863762e1dea5e76ab207c091
ezreal <956371950@qq.com>
2023-06-17 10:36:49 +0800

    init: init project

---
+++ src/main/java/com/ezreal/autobi/AutoBiApplication.java
@@ -0,0 +1,13 @@
+package com.ezreal.autobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoBiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoBiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ezreal/autobi/AutoBiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezreal.autobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
